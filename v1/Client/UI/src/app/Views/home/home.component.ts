1290dd3586450882cc277734d0ae92840e3de51d
DanielLaraSanchez <daniellaraedinburgh@hotmail.com>
2019-09-23 22:24:31 +0100

    it fucking works. now everything is called withing oninit and works asychonously automatically

--- v1/Client/UI/src/app/Views/home/home.component.ts
+++ v1/Client/UI/src/app/Views/home/home.component.ts
@@ -12,143 +12,193 @@ export class HomeComponent implements OnInit {
   client = {};
   remoteSocketId;
   peerConnection;
+  socket;
+  pairedPeerWaiting = false;
 
   constructor(public _webSocketService: WebsocketService) {
+    this.socket = this._webSocketService.socket;
   }
 
   ngOnInit() {
 
     // this.connect();
     // this.onOffer();
-
-  }
-
-  endCall(){
-    console.log("inside endcall")
-    
-    this._webSocketService.socket.emit('bye', this._webSocketService.socket.id);
-  }
-
-  onOffer() {
-
-    let socket = this._webSocketService.socket;
-    let video = document.createElement('video');
-    video.height = 200;
-    video.width = 200;
-    let videoPropioDiv = document.getElementById('video2Div');
-    videoPropioDiv.appendChild(video);
-    let peerConnection = new RTCPeerConnection();
-    socket.on('offer', function (id, description) {
-      peerConnection.setRemoteDescription(description)
-        .then(() => peerConnection.createAnswer())
-        .then(sdp => peerConnection.setLocalDescription(sdp))
-        .then(function () {
-          socket.emit('answer', id, peerConnection.localDescription);
-        });
-      peerConnection.ontrack = function (event) {
-        console.log(event.streams)
-        video.srcObject = event.streams[0];
-        video.play()
-      };
-      peerConnection.onicecandidate = function (event) {
-        if (event.candidate) {
-          console.log(event.candidate, "event")
-          socket.emit('candidate', id, event.candidate);
-        }
-      };
-    })
-
-    socket.on('candidate', function (id, candidate) {
-      console.log(candidate, peerConnection, "insidecandidate")
-      peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
-        .catch(e => console.error(e));
-    });
-
-    // socket.on('connect', function () {
-    //   socket.emit('watcher');
-    // });
-
-    socket.on('broadcaster', function () {
-      socket.emit('watcher');
-    });
-
-    socket.on('bye', function () {
-      peerConnection.close();
-      console.log("dentro de bye")
-    });
-
-  }
-
-  test(){
-    var constraints = { audio: false, video: { width: 1280, height: 720 } };
-
-    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-     console.log(stream)
-    }).catch(error => console.error(error));
-
-  }
-
-  sendOffer() {
-    let socket = this._webSocketService.socket;
-    let video = document.createElement('video');
-    let div = document.getElementById('video1Div');
-    video.height = 200;
-    video.width = 200;
-    video.style.backgroundColor = "black"
-    div.appendChild(video);
-    const peerConnections = {};
-    if(Object.keys(peerConnections).length < 2){
-    var constraints = { audio: false, video: { width: 1280, height: 720 } };
-    navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-        socket.emit('broadcaster');
-      }).catch(error => console.error(error));
-
-    socket.on('answer', function (id, description) {
-      peerConnections[id].setRemoteDescription(description);
-    });
+    this.onOfferNuevo(this.onOffer, this.sendOffer);
+  
+this.readyToBattle();  }
 
 
-    socket.on('watcher', function (id) {
-      const peerConnection = new RTCPeerConnection();
-      console.log(peerConnections, "peerconnections")
-      peerConnections[id] = peerConnection;
-      let stream = video.srcObject;
-      (<MediaStream>stream).getTracks().forEach(track => {
-        peerConnection.addTrack(track, <MediaStream>stream)
+
+
+
+
+    onOffer(socket) {
+      const peerConnections = {};
+      let peerConnection = new RTCPeerConnection();
+      let video = document.createElement('video');
+      let div = document.getElementById('video1Div');
+      video.height = 200;
+      video.width = 200;
+      video.style.backgroundColor = "black"
+      div.appendChild(video);
+      //////////////////////////////////////////////////
+      let videoOtherPeer = document.createElement('video');
+      videoOtherPeer.height = 200;
+      videoOtherPeer.width = 200;
+      let videoOtherPeerDiv = document.getElementById('video2Div');
+      videoOtherPeerDiv.appendChild(videoOtherPeer);
+      var constraints = { audio: false, video: { width: 1280, height: 720 } };
+      navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+          video.srcObject = stream;
+          video.play();
+          socket.emit('broadcaster');
+        }).catch(error => console.error(error));
+
+      ///////////////////////////////////////////////////    var constraints = { audio: false, video: { width: 1280, height: 720 } };
+      socket.on('offer', function (id, description) {
+        peerConnection.setRemoteDescription(description)
+          .then(() => peerConnection.createAnswer())
+          .then(sdp => peerConnection.setLocalDescription(sdp))
+          .then(function () {
+            
+            socket.emit('answer', id, peerConnection.localDescription);
+          });
+          let stream = video.srcObject;
+
+          (<MediaStream>stream).getTracks().forEach(track => {
+            peerConnection.addTrack(track, <MediaStream>stream)
+          });
+         
+        peerConnection.ontrack = function (event) {
+          console.log(event.streams)
+          videoOtherPeer.srcObject = event.streams[0];
+          videoOtherPeer.play()
+        };
+        peerConnection.onicecandidate = function (event) {
+          if (event.candidate) {
+            console.log(event.candidate, "event")
+            socket.emit('candidate', id, event.candidate);
+          }
+        };
+      })
+  
+      socket.on('candidate', function (id, candidate) {
+        console.log(candidate, peerConnection, "insidecandidate")
+        peerConnection.addIceCandidate(new RTCIceCandidate(candidate))
+          .catch(e => console.error(e));
       });
-      peerConnection.createOffer().then(sdp => peerConnection.setLocalDescription(sdp)).then(function () {
-          socket.emit('offer', id, peerConnection.localDescription);
+      socket.on('broadcaster', function () {
+        socket.emit('watcher');
+      });
+  
+      socket.on('bye', function () {
+        peerConnection.close();
+        console.log("dentro de bye")
+      });
+  
+  
+    }
+    sendOffer(socket) {
+      const peerConnections = {};
+
+      let video = document.createElement('video');
+      let div = document.getElementById('video1Div');
+      video.height = 200;
+      video.width = 200;
+      video.style.backgroundColor = "black"
+      div.appendChild(video);
+      ///////////////////////////////////////
+      let videoOtherPeer = document.createElement('video');
+      videoOtherPeer.height = 200;
+      videoOtherPeer.width = 200;
+      let videoOtherPeerDiv = document.getElementById('video2Div');
+      videoOtherPeerDiv.appendChild(videoOtherPeer);
+      if(Object.keys(peerConnections).length < 2){
+      var constraints = { audio: false, video: { width: 1280, height: 720 } };
+      navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+          video.srcObject = stream;
+          video.play();
+          socket.emit('broadcaster');
+        }).catch(error => console.error(error));
+  
+      socket.on('answer', function (id, description) {
+        peerConnections[id].setRemoteDescription(description);
+      });
+  
+  
+      socket.on('watcher', function (id) {
+        const peerConnection = new RTCPeerConnection();
+        console.log(peerConnections, "peerconnections")
+        peerConnections[id] = peerConnection;
+        let stream = video.srcObject;
+        (<MediaStream>stream).getTracks().forEach(track => {
+          peerConnection.addTrack(track, <MediaStream>stream)
         });
-      peerConnection.onicecandidate = function (event) {
-        if (event.candidate) {
-          socket.emit('candidate', id, event.candidate);
-        }
-      };
+        peerConnection.createOffer().then(sdp => peerConnection.setLocalDescription(sdp)).then(function () {
+            socket.emit('offer', id, peerConnection.localDescription);
+          });
+        peerConnection.onicecandidate = function (event) {
+          console.log(event.candidate, "candidate inside sender onicecanddate")
+          if (event.candidate) {
+            socket.emit('candidate', id, event.candidate);
+          }
+        };
+  
+        peerConnection.ontrack = function (event) {
+          console.log(event.streams)
+          videoOtherPeer.srcObject = event.streams[0];
+          videoOtherPeer.play()
+        };
+      });
+  
+  
+      socket.on('candidate', function (id, candidate) {
+        console.log(candidate, "insidecandidate sender")
+        peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+      });
+  
+      socket.on('bye', function (id) {
+        console.log("inside bye onoffer")
+        peerConnections[id] && peerConnections[id].close();
+        delete peerConnections[id];
+        video.hidden = true;
+      });
+    }
 
-      peerConnection.ontrack = function (event) {
-        console.log(event.streams)
-        video.srcObject = event.streams[0];
-        video.play()
-      };
+    
+    }
+
+  
+
+  
+
+
+
+
+   onOfferNuevo(onOfferCallback, sendOfferCallback){
+
+    let socket = this.socket;
+    socket.on('responsability', function(responsability){
+      console.log(responsability, "funciona from client")
+      if(responsability === 'activateOnOffer'){
+        onOfferCallback(socket);
+        socket.emit('clientIsWaiting', socket.id)
+      }else if(responsability === 'activateSendOffer'){
+        sendOfferCallback(socket)
+      }else{
+        console.log('esperando que se conecte peer')
+
+      }
+     
     });
-
-
-    socket.on('candidate', function (id, candidate) {
-      console.log(candidate, "insidecandidate sender")
-
-      peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
-    });
-
-    socket.on('bye', function (id) {
-      console.log("inside bye onoffer")
-      peerConnections[id] && peerConnections[id].close();
-      delete peerConnections[id];
-      video.hidden = true;
-    });
+    
   }
-  }
+
+ readyToBattle(){
+  
+   this.socket.emit('readyToBattle', this._webSocketService.socket.id)
+  
+ }
 
 
   // sendOffer() {
--- v1/Server/server.js
+++ v1/Server/server.js
@@ -1,50 +1,83 @@
-
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 server.listen(3000, () => {
   console.log("socket.io server is listening on port 3000");
-})
+});
 
 let broadcaster;
-let numberOfConnections = 0;
 const clients = [];
-console.log(clients)
+const clientsReadyToBattle = [];
 
-function createRooms(socketid){
-  if(clients.length === 0){
-    this.numberOfConnections = 1;
-    clients.push({[socketid] :  null})
-  }else{
-     findClientWaiting(socketid)
-    
-    this.numberOfConnections++;
+
+function createRooms(socketid) {
+  if (clients.length <= 1) {
+    clientsReadyToBattle.push({ peer1: socketid, peer2: null, peerWaiting: false })
+  } else {
+    createPairsObject(socketid)
   }
-  console.log(this.numberOfConnections, "26", clients)
-
-  return this.numberOfConnections
 }
 
-function findClientWaiting(newSocketId){
-  clients.forEach(function(client){
-    console.log(clients[Object.keys(client)[0]], "insidefindclient")
+function createPairsObject(newSocketId) {
+  const peerWaiting = clientsReadyToBattle.filter(function (client) {
+    return client.peer2 === null || client.peer1 === null;
+  });
+  if (peerWaiting.length === 0) {
+    clientsReadyToBattle.push({ peer1: newSocketId, peer2: null, peerWaiting: false })
+  } else {
+    peerWaiting[0].peer2 = newSocketId;  //why is this working? peerWaiting is a new array. but it is modifying clientsReadyToBattle.
+  }
+}
 
-    if(clients[Object.keys(client)[0]] === null){
-      Object.keys(client)[0] = newSocketId
-      console.log(Object.keys(client)[0], "insidefindclient")
+function getResponsability(socketid) {
+  let responsability;
+  clientsReadyToBattle.forEach(function (pairOfPeers) {
+    let resp;
+    // if (pairOfPeers.peer1 === socketid && pairOfPeers.peer2 != null) {
+      if ((pairOfPeers.peerWaiting === false) && (pairOfPeers.peer1 === socketid || pairOfPeers.peer2 === socketid)) {
+      resp = 'activateOnOffer';
+    }
+    else if ((pairOfPeers.peer2 === socketid || pairOfPeers.peer1 === socketid ) && (pairOfPeers.peerWaiting === true)) {//why does this not work with just and if statement. why do i have to go else if. what is the real difference between them two
+      resp = 'activateSendOffer';
+    }  else if ((pairOfPeers.peer2 === socketid || pairOfPeers.peer1 === socketid) && (pairOfPeers.peer1 === null || pairOfPeers.peer2 === null ) && pairOfPeers.peerWaiting === false){
+      resp = 'activateOnOffer';
+    }else{
+      resp = false;
+      console.log("why? linea47", pairOfPeers)
+
+      }
+      console.log("funciona linea49", clientsReadyToBattle)
+    responsability = resp
+  });
+  return responsability
+}
+
+function setPeerWaitingTrue(socketid){
+  clientsReadyToBattle.forEach(function(pairObject){
+    if(pairObject.peer1 === socketid || pairObject.peer2 === socketid){
+      pairObject.peerWaiting = true;
     }
   })
+  console.log(clientsReadyToBattle, "from setpeerwiatingtrue linea61")
 }
 
-
 io.sockets.on('connection', function (socket) {
-  this.numberOfConnections = createRooms(socket.id)
-  console.log(this.numberOfConnections, "35")
-  // clients.push({[socket.id]: "hola" })
+  clients.push(socket.id)
+  createRooms(socket.id)
+  console.log(clientsReadyToBattle, "clientsReadyToBattle array linea67")
   console.log('Currently there are ' + clients.length + ' connected', clients);
 
+  socket.on('readyToBattle', function (socketid) {
+    const responsability = getResponsability(socket.id)
+    socket.emit('responsability', responsability)
+    // console.log(responsability, "clientsReadyToBattle",clientsReadyToBattle[0].peer2, socket.id, clientsReadyToBattle)
+  })
 
+  socket.on('clientIsWaiting', function (socketid){
+    console.log("funciona clientis waiting linea 77", socketid)
+    setPeerWaitingTrue(socketid)
+  })
 
   socket.on('broadcaster', function () {
     broadcaster = socket.id;
@@ -56,14 +89,21 @@ io.sockets.on('connection', function (socket) {
     socket.to(broadcaster).emit('watcher', socket.id);
   });
   socket.on('offer', function (id /* of the watcher */, message) {
-    socket.to(id).emit('offer', socket.id /* of the broadcaster */, message);
+    // console.log(id, "id offer")
+    if(id != socket.id){
+      socket.to(id).emit('offer', socket.id /* of the broadcaster */, message);
+
+    }
   });
   socket.on('answer', function (id /* of the broadcaster */, message) {
     socket.to(id).emit('answer', socket.id /* of the watcher */, message);
 
   });
   socket.on('candidate', function (id, message) {
-    socket.to(id).emit('candidate', socket.id, message);
+    if(id != socket.id){
+      socket.to(id).emit('candidate', socket.id, message);
+
+    }
   });
 
 
@@ -78,11 +118,22 @@ io.sockets.on('connection', function (socket) {
         clients.splice(clients.indexOf(socket.id), 1)
       }
     })
+    clientsReadyToBattle.forEach(function(pairOfPeers){
+      if(pairOfPeers.peer1 === socket.id){
+        pairOfPeers.peer1 = null
+      }
+      if(pairOfPeers.peer2 === socket.id){
+        pairOfPeers.peer2 = null
+      }
+      if(pairOfPeers.peer1 === null && pairOfPeers.peer2 === null){
+        clientsReadyToBattle.splice(clientsReadyToBattle.indexOf(pairOfPeers))
+      }
+    })
+    console.log(clientsReadyToBattle, "clientsReadyToBattle on disconnect")
     console.log("client" + socket.id, "has disconnected, currently there are", clients.length, "connected")
   });
 
   socket.on('bye', function (id) {
-    console.log("inside bye ", id)
     broadcaster && socket.to(broadcaster).emit('bye', id)
   })
 });
@@ -173,6 +224,188 @@ var addNewUsersToClientsArray = function (newClient) {
 
   }
 }
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+/////copy
+// const express = require('express');
+// const app = express();
+// const server = require('http').Server(app);
+// const io = require('socket.io')(server);
+// server.listen(3000, () => {
+//   console.log("socket.io server is listening on port 3000");
+// });
+
+// let broadcaster;
+// const clients = [];
+// const clientsReadyToBattle = [];
+
+
+// function createRooms(socketid){
+//   if(clientsReadyToBattle.length === 0){
+//     clientsReadyToBattle.push({ peer1 :  socketid, peer2: null})
+//   }else{
+//      findClientWaiting(socketid)
+//   }
+// }
+
+// function findClientWaiting(newSocketId){
+//   const peerWaiting = clientsReadyToBattle.filter(function(client){
+//     return client.peer2 === null;
+//   });
+//   if(peerWaiting.length === 0){
+//     clientsReadyToBattle.push({peer1:newSocketId, peer2:null})
+//   }else{
+//     peerWaiting[0].peer2 = newSocketId;
+//   }
+// }
+
+
+// io.sockets.on('connection', function (socket) {
+//   clients.push(socket.id)
+//   // createRooms(socket.id)
+//   console.log('Currently there are ' + clients.length + ' connected', clients);
+
+//   socket.on('readyToBattle', function(socketid){
+//     createRooms(socketid)
+//     console.log(clientsReadyToBattle, "clientsReadyToBattle")
+//   })
+
+//   socket.on('broadcaster', function () {
+//     broadcaster = socket.id;
+//     socket.broadcast.emit('broadcaster');
+//   });
+
+//   socket.on('watcher', function () {
+//     // broadcaster && socket.to(broadcaster).emit('watcher', socket.id);
+//     socket.to(broadcaster).emit('watcher', socket.id);
+//   });
+//   socket.on('offer', function (id /* of the watcher */, message) {
+//     socket.to(id).emit('offer', socket.id /* of the broadcaster */, message);
+//   });
+//   socket.on('answer', function (id /* of the broadcaster */, message) {
+//     socket.to(id).emit('answer', socket.id /* of the watcher */, message);
+
+//   });
+//   socket.on('candidate', function (id, message) {
+//     socket.to(id).emit('candidate', socket.id, message);
+//   });
+
+
+
+
+
+
+//   socket.on('disconnect', function (message) {
+//     broadcaster && socket.to(broadcaster).emit('bye', socket.id);
+//     clients.forEach(function (client) {
+//       if (client === socket.id) {
+//         clients.splice(clients.indexOf(socket.id), 1)
+//       }
+//     })
+//     console.log("client" + socket.id, "has disconnected, currently there are", clients.length, "connected")
+//   });
+
+//   socket.on('bye', function (id) {
+//     console.log("inside bye ", id)
+//     broadcaster && socket.to(broadcaster).emit('bye', id)
+//   })
+// });
+
+// io.on('connect', function (socket) {
+//     var newUserObject = {
+//         id: socket.id,
+//         nickname: null
+//     }
+//     addNewUsersToClientsArray(newUserObject)
+//     var allConnectedClients = Object.keys(io.sockets.connected);
+//     console.log("new user has connected, their Id is: " + socket.id + " #Currently there are " + clients.length + " users connected.")
+//     socket.on('nickname', function (nickname) {
+//         socket.nickname = nickname;
+//         newUserObject.nickname = nickname
+//         io.emit('userslistonConnection', clients)
+//     });
+//     socket.on('disconnect', function () {
+//         removeClientThatDisconnected(socket.id)
+//         console.log("new user has disconnected, their Id is: " + socket.id + " #Currently there are " + clients.length + " users connected.", 36)
+//         io.emit('userslistonDisconnection', clients)
+//     });
+
+//     socket.on('offer', function(id, socket){
+//         let remoteSocket = allClientsButYou(socket);
+//         console.log(id, socket)
+//         io.to(remoteSocket.id).emit('offer', id/* of the broadcaster */, socket);
+//     })
+
+//     socket.on('answer', function(id, socket, localSocketId){
+//         console.log(id, socket, "id socket")
+//         io.to(id).emit('answer', id/* of the broadcaster */, socket, localSocketId);
+
+//     })
+
+//     socket.on('candidate', function(id, candidates){
+//         console.log(socket.id, "oncandidate ", id)
+//         io.to(id).emit('candidate',socket.id, candidates)
+//     })
+
+//  });
+
+
+////////////////////////////////////////////////////
+
+
+
+
+
+
+
+// var allClientsButYou = function (socket) {
+//   let otherClients = clients.filter((client) => {
+//     return client.id != socket
+//   })
+
+//   return otherClients[0];
+// }
+
+
+// //TOOLS         
+
+// var removeClientThatDisconnected = function (id) {
+//   if (clients.length === 1) {
+//     clients.pop();
+//   } else {
+//     clients.forEach(function (client) {
+//       console.log(client.id, id)
+//       if (client.id === id) {
+//         clients.splice(client, 1)
+//       }
+//     });
+//   }
+// }
+
+// var addNewUsersToClientsArray = function (newClient) {
+//   if (clients.length === 0) {
+//     clients.push(newClient)
+//   } else {
+//     var counter = 0;
+//     clients.forEach(function (client) {
+//       if (client.id === newClient.id) {
+//         counter++
+//       }
+//     })
+//   } if (counter === 0) {
+//     clients.push(newClient)
+
+//   }
+// }
 
 
 
